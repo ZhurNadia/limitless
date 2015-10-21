e345b091c84d6403b84bf8adafaab76b3e5ad956
Zeka <asidzeka@gmail.com>
2015-10-21 16:18:20 +0300

    structure change + gulp + sass

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+// Concat css
+gulp.task('concat', function () {
+    gulp.src('./assets/css/*.css')
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./build/css/'))
+});
+
+// Compile SASS
+gulp.task('sass', function () {
+    gulp.src('./assets/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'));
+});
+
+
+// Minify JS
+gulp.task('uglify', function() {
+    gulp.src('./assets/js/*.js')
+        .pipe(concat('index.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'));
+});
+
+
+gulp.task('build', function() {
+    // concat css
+    gulp.run('concat');
+    gulp.run('sass');
+    gulp.run('uglify');
+
+
+
+
+
+
+    //// image
+    //gulp.src('./assets/images/*')
+    //    .pipe(imagemin())
+    //    .pipe(gulp.dest('./build/img'))
+
+});
